25587622ed8f81d690672524eea31345f13bd103
ZXChadd <zhexianchadd@gmail.com>
2017-05-31 11:50:12 +0800

    Update CSS

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tweets do
-    resources :replies
+    resource :reply
   end
-
+  
   root 'tweets#home'
 
 end
