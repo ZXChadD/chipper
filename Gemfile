934b47b42ce5f4f96e6b41563b638507b2286e84
ZXChadd <zhexianchadd@gmail.com>
2017-06-05 01:15:50 +0800

    Add seed data

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'uglifier', '>= 1.3.0'
 
 #new gems
 gem 'devise'
+gem 'faker', '~> 1.4', '>= 1.4.3'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+     User.create(
+        email: Faker::Internet.email,
+        password: 123456
+      )
+end
+
+
+10.times do
+     Tweet.create(
+        body: Faker::Lorem.sentence,
+        user_id: 12
+      )
+end
