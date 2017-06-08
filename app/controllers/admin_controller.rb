55ef6a85e407cea527095030633e168050fc7b3f
hadi91 <ahmad_nurhadi@hotmail.com>
2017-06-08 16:32:25 +0800

    add admin index route

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,7 @@
 class AdminController < ApplicationController
   before_action :authenticate_user!
   before_action :is_admin?
+  before_action :find_user, only: [:destroy,]
 
   def index
     @users = User.all
@@ -12,6 +13,11 @@ class AdminController < ApplicationController
 
   def destroy
     @user.destroy!
+  end
+
+  private
+  def find_user
+    @user = User.find(params[:id])
   end
 
 end
